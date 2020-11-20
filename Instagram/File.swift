a72e45b4f551883c91c81ee5c77c0ec529405203
ryosuke.watanabe <genmaiokomey@yahoo.co.jp>
2020-11-20 14:58:39 +0900

    ログイン画面の作成

---
+++ Instagram/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by 渡辺涼介 on 2020/11/19.
+//  Copyright © 2020 ryosuke.watanabe. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by 渡辺涼介 on 2020/11/19.
+//  Copyright © 2020 ryosuke.watanabe. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
